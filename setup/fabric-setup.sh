24a6d02f57870cdb29c50b277cc609a8c970672a
Adnan Iftekhar <adnan.iftekhar.ch@gmail.com>
2018-12-18 16:18:04 +0800

    Update Hyperledger setup scripts

--- setup/fabric-setup.sh
+++ setup/fabric-setup.sh
@@ -18,8 +18,8 @@ sudo cp fabric-samples/bin/*    /usr/local/bin
 
 echo "### Copy the binaries from fabric-samples/bin to bin folder for future use ###"
 
-mkdir bin
-cp fabric-samples/bin/*    bin
+mkdir -p ${PWD}/../bin
+cp fabric-samples/bin/*    ${PWD}/../bin
 
 # The sample chaincode is under the subfolder go and need to come under gopath/src subfolder
 
@@ -31,9 +31,15 @@ echo "### copying chaincodes from fabric-samples folder in gopath/src folder ###
 
 cp -r fabric-samples/chaincode/chaincode_example02/*    $GOPATH/src
 
+# Move fabric-samples to hyperledger-1.3.0 directory
+
+mv fabric-samples ${PWD}/../ 
+
 # This downloads the shim code 
 
 echo "### Setting up the HLF Shim ###"
 
 mkdir -p  $GOPATH/src/github.com/hyperledger
 go get -u --tags nopkcs11 github.com/hyperledger/fabric/core/chaincode/shim
+
+echo "### Installaion is completed ###"
